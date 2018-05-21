a2fbef093c536e18b044aaa0e175a4f94c3c6f0e
John <ristaujo@gmail.com>
2018-05-21 16:31:59 -0400

    fixed positioning issue

--- controller.js
+++ controller.js
@@ -3,32 +3,7 @@
   function toDoCtrl() {
     const vm = this;
 
-    vm.items = [
-      {
-        task: "Grocery Shopping",
-        completed: true,
-      },
-      {
-        task: "Oil Change",
-        completed: false,
-      },
-      {
-        task: "Cut the Grass",
-        completed: true,
-      },
-      {
-        task: "Make Beats",
-        completed: false,
-      },
-      {
-        task: "Feed Cats",
-        completed: true,
-      },
-      {
-        task: "Clean Bathroom",
-        completed: false,
-      }
-    ];
+    vm.items = [];
     vm.addTask = (task, completed) => {
       vm.items.push({
         task: vm.item.task,
