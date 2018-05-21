fd280a67d01c1d278b5d2995fceef796e824f1ef
John <ristaujo@gmail.com>
2018-05-21 16:19:36 -0400

    Added part two functionality

--- controller.js
+++ controller.js
@@ -29,10 +29,26 @@
         completed: false,
       }
     ];
+    vm.addTask = (task, completed) => {
+      vm.items.push({
+        task: vm.item.task,
+        completed: false
+      })
+      vm.item = {};
 
-  };
 
+    };
+    vm.removeTask = (index, task) => {
+      vm.items.splice(index, 1);
+    
+    };
+
+    vm.completeTask = (index, completed) => {
+      vm.items[index].completed = true
+    }
+  }
   angular
     .module("todoApp")
     .controller("toDoCtrl", toDoCtrl);
-} 
+
+}
